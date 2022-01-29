a9b095ae74d26e1fcab8f6acfcea1c6279c1f88b
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-01-29 20:12:37 +1000

    Fix

--- src/test/java/ru/netology/bonus/BonusServiceTest1.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest1.java
@@ -2,7 +2,6 @@ package ru.netology.bonus;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest1 {
